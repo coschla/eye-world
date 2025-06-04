ba62010fdc6ede90b48326b5d4ee490f43465d88
coschla <coschla@okstate.edu>
2025-06-04 13:18:48 -0500

    cleaned code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import yaml
-
+from data.write import write_eye_gaze_dataset
 from data.process import process_gaze_data
 from utils import skip_run
+
+
 
 # The configuration file
 config_path = "configs/config.yaml"
@@ -12,3 +14,5 @@ with skip_run("run", "data_cleaning") as check, check():
 
     for game, valid_actions in games.items():
         process_gaze_data(game, config)
+
+        write_eye_gaze_dataset(file_path=None)
